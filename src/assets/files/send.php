494cb2ab8e65af87c978901f701b8709daedacf2
SBurnt <zip2004zip@gmail.com>
2022-04-25 12:01:07 +0300

    edit content

--- src/assets/files/send.php
+++ src/assets/files/send.php
@@ -3,11 +3,13 @@
 $username = $_POST['name'];
 $usertel = $_POST['tel'];
 $useremail = $_POST['email'];
-$typeCourse = $_POST['type'];
+$courses = $_POST['courses'];
+$typeCourses = $_POST['type'];
 
 echo ' username ' . $username;
 echo ' usertel ' .  $usertel;
 echo ' useremail ' .  $useremail;
+echo ' courses ' .  $courses;
 echo ' typeCourse ' .  $typeCourse;
 
-require_once __DIR__ . "/telegram.php"; // Send to Telegram
+// require_once __DIR__ . "/telegram.php"; // Send to Telegram
